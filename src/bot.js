dd04e13680144ad956d7aad3dea5b9fe4e73ead2
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-02 16:03:58 +0300

    fuck

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,13 @@
 import { Bot } from "grammy";
+import { getTLTPrice } from "./core/strategy";
 
 const TOKEN = "8583323595:AAGZ8NQ4YpFHPO_1o67czZqX1z4LftsSkbU";
 const BOT = new Bot(TOKEN);
 
-BOT.command("start", (ctx) => ctx.reply("Hi! I'm the TLT Bot."));
+BOT.command("start", (ctx) => {
+  ctx.reply("Hi! I'm the TLT Bot.");
+  getTLTPrice();
+});
 
 BOT.on("message:text", (ctx) => {
   ctx.reply(`You wrote: ${ctx.message.text}`);
