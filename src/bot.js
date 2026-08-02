56f3938ff7ad39a82733962e57cbf2cd1e2d1abf
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-02 16:14:15 +0300

    try again

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,9 @@
 import { Bot } from "grammy";
-import { getTLTPrice } from "./core/strategy";
 
 const TOKEN = "8583323595:AAGZ8NQ4YpFHPO_1o67czZqX1z4LftsSkbU";
 const BOT = new Bot(TOKEN);
+
+const yahooFinance = new YahooFinance();
 
 BOT.command("start", (ctx) => {
   ctx.reply("Hi! I'm the TLT Bot.");
@@ -14,4 +15,5 @@ BOT.on("message:text", (ctx) => {
 });
 
 BOT.start();
+
 console.log("BOT запущен");
--- src/core/strategy.js
+++ src/core/strategy.js
@@ -1,14 +1,1 @@
 // принимает решение
-import YahooFinance from "yahoo-finance2";
-
-const yahooFinance = new YahooFinance();
-
-export async function getTLTPrice() {
-  try {
-    const quote = await yahooFinance.quote("TLT");
-    console.log("Цена TLT:", quote.regularMarketPrice);
-    return quote.regularMarketPrice;
-  } catch (error) {
-    console.error("Ошибка получения цены:", error);
-  }
-}
