4e6f1954f0cc47d2ac4ababcb89b5cd8c8cd656f
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-02 17:42:39 +0300

    add price and dividends

--- src/bot.js
+++ src/bot.js
@@ -1,19 +1,13 @@
 import { Bot } from "grammy";
+import getData from "./replies/replies.js";
 
 const TOKEN = "8583323595:AAGZ8NQ4YpFHPO_1o67czZqX1z4LftsSkbU";
 const BOT = new Bot(TOKEN);
 
-const yahooFinance = new YahooFinance();
-
-BOT.command("start", (ctx) => {
-  ctx.reply("Hi! I'm the TLT Bot.");
-  getTLTPrice();
-});
-
-BOT.on("message:text", (ctx) => {
-  ctx.reply(`You wrote: ${ctx.message.text}`);
+BOT.command("data", getData);
+BOT.catch((err) => {
+  console.error("Global error: ", err);
 });
 
 BOT.start();
-
 console.log("BOT запущен");
--- src/replies/replies.js
+++ src/replies/replies.js
@@ -1,1 +1,14 @@
 // действия бота (тг)
+import { getTLTData } from "../core/strategy.js";
+
+export default async function getData(ctx) {
+  try {
+    const data = await getTLTData();
+
+    let reply = `Цена TLT: $${data.price}\nКупоны: ${data.lastDividend}, ${data.lastExDate}`;
+    await ctx.reply(reply);
+  } catch (err) {
+    await ctx.reply("❌ Ошибка получения данных");
+    console.error(err);
+  }
+}
