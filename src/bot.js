fea124be5619b97508f2cd0620fc4f9385ac4027
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-02 15:04:26 +0300

    first steps

---
+++ src/bot.js
@@ -0,0 +1,13 @@
+import { Bot } from "grammy";
+
+const TOKEN = "8583323595:AAGZ8NQ4YpFHPO_1o67czZqX1z4LftsSkbU";
+const BOT = new Bot(TOKEN);
+
+BOT.command("start", (ctx) => ctx.reply("Hi! I'm the TLT Bot."));
+
+BOT.on("message:text", (ctx) => {
+  ctx.reply(`You wrote: ${ctx.message.text}`);
+});
+
+BOT.start();
+console.log("BOT запущен");
