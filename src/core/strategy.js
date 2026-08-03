b683d4a57f56850c539c29595dc02f25630f50cd
darloh1ng <discussavod@mail.ru>
2026-08-03 22:12:08 +0300

    strategy new

--- src/core/strategy.js
+++ src/core/strategy.js
@@ -1,1 +1,26 @@
 // принимает решение
+let TLT = 182.28
+let devidend = 0.318
+let FRS = 3.75
+let inflation = 3.5
+
+const buy = () => {
+  console.log(`покупаем облигации, потому что реальная доходность TLT ${realYield} > 0 и цена TLT ${TLT} < 85 и ставка ФРС ${FRS} > 3.5 (высокая)`)
+}
+const sell = () => {
+  console.log(`продаем облигации, потому что цена TLT ${TLT} > 99`)
+}
+const stay = () => {
+  console.log('ничего не делаем')
+}
+
+let nominalYield = (devidend / TLT * 12 * 100)
+let realYield = ((1 + nominalYield) / (1 + inflation) - 1)
+
+if (realYield > 0 && TLT < 85 && FRS > 3.5) {
+  buy()
+} else if (TLT > 99) {
+  sell()
+} else {
+  stay()
+}
