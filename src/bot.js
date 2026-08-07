58d48e17da3e7d733440018aaaba9a3e6f4ddfed
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-07 20:35:40 +0300

    a tak

--- src/bot.js
+++ src/bot.js
@@ -3,7 +3,7 @@ import getData from "./replies/replies.js";
 import { mainKeyboard } from "./keyboards/keyboards.js";
 import dotenv from "dotenv";
 import express from "express";
-// import { startDailyTasks } from "./core/dataProvider.js";
+import { startDailyTasks } from "./core/dataProvider.js";
 dotenv.config();
 
 const TOKEN = process.env.BOT_TOKEN;
@@ -48,7 +48,7 @@ const PORT = process.env.PORT || 3000;
 
 const isProduction = process.env.NODE_ENV === "production";
 
-// startDailyTasks();
+startDailyTasks();
 
 if (isProduction) {
   app.listen(PORT, async () => {
