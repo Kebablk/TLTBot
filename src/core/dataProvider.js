74fffe5acd3d391e10c89ce9120b1137cdebe18c
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-09 01:29:06 +0300

    change time

--- src/core/dataProvider.js
+++ src/core/dataProvider.js
@@ -81,7 +81,7 @@ export async function saveClose() {
 }
 
 export function startDailyTasks() {
-  cron.schedule("27 21 * * *", saveOpen);
+  cron.schedule("30 21 * * *", saveOpen);
   cron.schedule("0 20 * * *", saveClose);
   console.log(
     "⏳ Планировщик запущен: open в 17:43 UTC (20:43 МСК), close в 20:00 UTC (23:00 МСК)",
