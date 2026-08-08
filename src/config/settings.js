cf725cd27c214bb92922f019bfc10f754096b5f6
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-08 23:56:57 +0300

    fix again

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,12 +1,17 @@
 import { getAllData } from "../core/dataProvider.js";
+import prisma from "../lib/prismaClient.js";
 
-const lastData = getAllData()[0];
+const lastData = await prisma.dailyData.findFirst({
+  orderBy: { date: "desc" },
+});
 
-const openTLT = lastData.openTLT;
-const closeTLT = lastData.closeTLT;
-const devidend = lastData.devidend;
-const inflation = lastData.inflation;
-const fedRate = lastData.fedRate;
+if (lastData) {
+  const openTLT = lastData.openTLT;
+  const closeTLT = lastData.closeTLT;
+  const devidend = lastData.devidend;
+  const inflation = lastData.inflation;
+  const fedRate = lastData.fedRate;
+}
 
 export const nominalYield = (devidend / closeTLT) * 12 * 100; // номинальная доходность
 export const realYeiel = ((1 + nominalYield) / (1 + inflation / 100) - 1) * 100; // реальная доходность
