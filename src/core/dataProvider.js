844ca8a6427dfa708c4d72a4583495c6fbed153e
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-09 00:21:05 +0300

    fix

--- src/core/dataProvider.js
+++ src/core/dataProvider.js
@@ -64,31 +64,31 @@ export async function saveClose() {
 }
 
 export function startDailyTasks() {
-  cron.schedule("17 3 * * *", saveOpen);
+  cron.schedule("30 13 * * *", saveOpen);
   cron.schedule("0 20 * * *", saveClose);
   console.log(
     "⏳ Планировщик запущен: open в 17:43 UTC (20:43 МСК), close в 20:00 UTC (23:00 МСК)",
   );
 }
 
-export async function getFirstData() {
-  const lastData = await prisma.dailyData.findFirst({
-    orderBy: { date: "desc" },
-  });
+// export async function getFirstData() {
+//   const lastData = await prisma.dailyData.findFirst({
+//     orderBy: { date: "desc" },
+//   });
 
-  const openTLT = lastData?.openTLT ?? 0;
-  const closeTLT = lastData?.closeTLT ?? 0;
-  const devidend = lastData?.devidend ?? 0;
-  const inflation = lastData?.inflation ?? 0;
-  const fedRate = lastData?.fedRate ?? 0;
+//   const openTLT = lastData?.openTLT ?? 0;
+//   const closeTLT = lastData?.closeTLT ?? 0;
+//   const devidend = lastData?.devidend ?? 0;
+//   const inflation = lastData?.inflation ?? 0;
+//   const fedRate = lastData?.fedRate ?? 0;
 
-  let nominalYield = 0;
-  let realYield = 0;
-  if (closeTLT !== 0 && dividend !== 0)
-    nominalYield = (dividend / closeTLT) * 12 * 100;
-  if (nominalYield !== 0 && inflation !== 0)
-    realYield = ((1 + nominalYield) / (1 + inflation / 100) - 1) * 100;
+//   let nominalYield = 0;
+//   let realYield = 0;
+//   if (closeTLT !== 0 && dividend !== 0)
+//     nominalYield = (dividend / closeTLT) * 12 * 100;
+//   if (nominalYield !== 0 && inflation !== 0)
+//     realYield = ((1 + nominalYield) / (1 + inflation / 100) - 1) * 100;
 
-  console.log(nominalYield, realYield);
-  return { nominalYield, realYield };
-}
+//   console.log(nominalYield, realYield);
+//   return { nominalYield, realYield };
+// }
