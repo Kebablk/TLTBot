f86e3681ab96f842c9cec793e2745246cf7f4c11
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-08 23:59:04 +0300

    change time

--- src/config/settings.js
+++ src/config/settings.js
@@ -11,7 +11,8 @@ if (lastData) {
   const devidend = lastData.devidend;
   const inflation = lastData.inflation;
   const fedRate = lastData.fedRate;
-}
 
-export const nominalYield = (devidend / closeTLT) * 12 * 100; // номинальная доходность
-export const realYeiel = ((1 + nominalYield) / (1 + inflation / 100) - 1) * 100; // реальная доходность
+  export const nominalYield = (devidend / closeTLT) * 12 * 100; // номинальная доходность
+  export const realYeiel =
+    ((1 + nominalYield) / (1 + inflation / 100) - 1) * 100; // реальная доходность
+}
