695701c2e0165ef3d30b01bdb46789790190e7da
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-12 00:50:06 +0300

    toFixed

--- src/replies/replies.js
+++ src/replies/replies.js
@@ -32,7 +32,7 @@ export default async function getData(ctx) {
       data.inflationRate,
     );
 
-    let reply = `📊 <b>API (сейчас)</b>:\n\n🏷️ Цена TLT: $${data.price}\n💰 Купон: ${data.lastDividend}, ${data.lastExDate}\n🏦 Ставка ФРС: ${data.fedRate}\n📈 Инфляция США: ${data.inflationRate}\n🗒️ nominalYield: ${APIYields.nominalYield}\n🗒️ realYield: ${APIYields.realYield}\n\n📁 <b>БД (последняя запись)</b>:\n\n📆 Дата: ${lastRecord.date}\n🔓 Открытие TLT: ${lastRecord.open}\n🔒 Закрытие TLT: ${lastRecord.close}\n💰 Купон: ${lastRecord.dividend}\n🏦 Ставка ФРС: ${lastRecord.fedRate}\n📈 Инфляция США: ${lastRecord.inflation}\n🗒️ nominalYield:  ${DBYields.nominalYield}\n🗒️ realYield: ${DBYields.realYield}`;
+    let reply = `📊 <b>API (сейчас)</b>:\n\n🏷️ Цена TLT: $${data.price.toFixed(2)}\n💰 Купон: ${data.lastDividend.toFixed(3)}, ${data.lastExDate}\n🏦 Ставка ФРС: ${data.fedRate.toFixed(2)}\n📈 Инфляция США: ${data.inflationRate.toFixed(2)}\n🗒️ nominalYield: ${APIYields.nominalYield.toFixed(2)}\n🗒️ realYield: ${APIYields.realYield.toFixed(2)}\n\n📁 <b>БД (последняя запись)</b>:\n\n📆 Дата: ${lastRecord.date}\n🔓 Открытие TLT: ${lastRecord.open}\n🔒 Закрытие TLT: ${lastRecord.close}\n💰 Купон: ${lastRecord.dividend}\n🏦 Ставка ФРС: ${lastRecord.fedRate}\n📈 Инфляция США: ${lastRecord.inflation}\n🗒️ nominalYield:  ${DBYields.nominalYield}\n🗒️ realYield: ${DBYields.realYield}`;
     return reply;
   } catch (err) {
     await ctx.reply("❌ Ошибка получения данных");
