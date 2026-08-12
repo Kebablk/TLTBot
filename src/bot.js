2d597631299a4149224aad79edd82fb691cce54e
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-12 19:06:15 +0300

    ?

--- src/bot.js
+++ src/bot.js
@@ -51,7 +51,6 @@ const isProduction = process.env.NODE_ENV === "production";
 
 (async function setHistory() {
   const data = await setTwoYearsData();
-  console.log(data);
 })();
 startDailyTasks();
 
