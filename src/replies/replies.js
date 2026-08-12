ccca73fccfe3654df0039363f836cef21da279ac
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-12 19:03:32 +0300

    alo

--- src/replies/replies.js
+++ src/replies/replies.js
@@ -3,7 +3,7 @@ import prisma from "../lib/prismaClient.js";
 import { getTLTData } from "./repliesStrategy.js";
 import { calculateYields } from "../config/settings.js";
 
-export default async function getData() {
+export default async function getData(ctx) {
   try {
     const data = await getTLTData();
     const lastRecord = await prisma.dailyData.findFirst({
@@ -33,7 +33,7 @@ export default async function getData() {
       data.inflationRate,
     );
 
-    let reply = `📊 <b>API (сейчас)</b>:\n\n🏷️ Цена TLT: $${data.price.toFixed(2)}\n💰 Купон: ${data.lastDividend.toFixed(3)}, ${data.lastExDate}\n🏦 Ставка ФРС: ${data.fedRate.toFixed(2)}\n📈 Инфляция США: ${data.inflationRate.toFixed(2)}\n🗒️ nominalYield: ${APIYields.nominalYield.toFixed(2)}\n🗒️ realYield: ${APIYields.realYield.toFixed(2)}\n\n📁 <b>БД (последняя запись)</b>:\n\n📆 Дата: ${lastRecord.date}\n🔓 Открытие TLT: ${lastRecord.open.toFixed(2)}\n🔒 Закрытие TLT: ${lastRecord.close.toFixed(2)}\n💰 Купон: ${lastRecord.dividend.toFixed(3)}\n🏦 Ставка ФРС: ${lastRecord.fedRate.toFixed(2)}\n📈 Инфляция США: ${lastRecord.inflation.toFixed(2)}\n🗒️ nominalYield:  ${DBYields.nominalYield.toFixed(2)}\n🗒️ realYield: ${DBYields.realYield.toFixed(2)}`;
+    let reply = `📊 <b>API (сейчас)</b>:\n\n🏷️ Цена TLT: $${data.price.toFixed(2)}\n💰 Купон: ${data.lastDividend.toFixed(3)}, ${data.lastExDate}\n🏦 Ставка ФРС: ${data.fedRate.toFixed(2)}\n📈 Инфляция США: ${data.inflationRate.toFixed(2)}\n🗒️ nominalYield: ${APIYields.nominalYield.toFixed(2)}\n🗒️ realYield: ${APIYields.realYield.toFixed(2)}\n\n📁 <b>БД (последняя запись)</b>:\n\n📆 Дата: ${lastRecord.date}\n🔓 Открытие TLT: ${lastRecord.open}\n🔒 Закрытие TLT: ${lastRecord.close}\n💰 Купон: ${lastRecord.dividend}\n🏦 Ставка ФРС: ${lastRecord.fedRate}\n📈 Инфляция США: ${lastRecord.inflation}\n🗒️ nominalYield:  ${DBYields.nominalYield}\n🗒️ realYield: ${DBYields.realYield}`;
     return reply;
   } catch (err) {
     await ctx.reply("❌ Ошибка получения данных");
