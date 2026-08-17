0c8d0916c7473f3538cf96080ab81f0a06911acb
Kebablk <glbuhmaterik2010@gmail.com>
2026-08-17 23:43:23 +0300

    fix and test logger

--- logs/logger.js
+++ logs/logger.js
@@ -8,9 +8,7 @@ const __dirname = path.dirname(__filename);
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || "info",
   format: winston.format.combine(
-    winston.format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss",
-    }),
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     winston.format.errors({ stack: true }),
     winston.format.splat(),
     winston.format.json(),
@@ -18,13 +16,24 @@ const logger = winston.createLogger({
   defaultMeta: { service: "tlt-bot" },
   transports: [
     new winston.transports.File({
-      filename: path.join(__dirname, "./error.log"),
+      filename: path.join(__dirname, "error.log"),
       level: "error",
     }),
     new winston.transports.File({
-      filename: path.join(__dirname, "./combined.log"),
+      filename: path.join(__dirname, "combined.log"),
     }),
   ],
 });
+
+// if (process.env.NODE_ENV !== "production") {
+//   logger.add(
+//     new winston.transports.Console({
+//       format: winston.format.combine(
+//         winston.format.colorize(),
+//         winston.format.simple()
+//       ),
+//     })
+//   );
+// }
 
 export default logger;
